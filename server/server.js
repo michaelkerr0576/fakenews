4d24d5bfbf5ba11dd64a47e52b9c797b9098fbde
michaelkerr0576 <53580213+michaelkerr0576@users.noreply.github.com>
2020-06-22 18:20:13 +0100

    Version 0.1.02
    Deployment fixes

--- server/server.js
+++ server/server.js
@@ -4,11 +4,16 @@ const app = require("./app");
 
 // Handle production build
 if (process.env.NODE_ENV === "production") {
+  console.log("HERE");
   // Static folder
-  app.use(express.static(__dirname + "./client/build"));
+  app.use(express.static(__dirname + "../client/build"));
   // Handle single page app
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + "./client/build/index.html"));
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + "../client/build/index.html"));
+  console.log("HERE");
 }
+
+//TEMPORY UNTIL BACKEND IS HOOKED UP
+app.use(express.static("../client/build"));
 
 const port = process.env.PORT || 5000;
 const server = http.createServer(app);
