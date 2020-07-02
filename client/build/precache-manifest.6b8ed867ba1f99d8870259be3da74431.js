75577e550fd76e5f0b2dfa64e6fec4abdbeb784e
michaelkerr0576 <53580213+michaelkerr0576@users.noreply.github.com>
2020-07-02 18:11:23 +0100

    Headline Component v2

---
+++ client/build/precache-manifest.6b8ed867ba1f99d8870259be3da74431.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffb6389eaaab3644127bfd7329caedc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6244a5f6c2a4459ae8d0",
+    "url": "/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "516843ba6c1d546d0f77",
+    "url": "/static/css/main.60dbabbd.chunk.css"
+  },
+  {
+    "revision": "6244a5f6c2a4459ae8d0",
+    "url": "/static/js/2.0433f040.chunk.js"
+  },
+  {
+    "revision": "ce8ac3c5d9da37689de19b45a8b43043",
+    "url": "/static/js/2.0433f040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "516843ba6c1d546d0f77",
+    "url": "/static/js/main.09dc64b2.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.358a3a5e6e47c02804f2f592026f9804.js"
+  "/precache-manifest.6b8ed867ba1f99d8870259be3da74431.js"
 );
 
 self.addEventListener('message', (event) => {
